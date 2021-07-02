8b33989da814eec00d96d736cb22662671880eb1
Ronny Bangsund <github@neuralshortcircuit.com>
2021-07-02 23:02:31 +0200

    Clarification.

--- count.go
+++ count.go
@@ -1,6 +1,14 @@
 package iterate
 
 // Count numbers for a range.
+// One argument returns a slice of numbers from 0 up (and ony up) to that number.
+// Two arguments return a slice ranging from the first to the second number (both inclusive, couunting up).
+// Three arhguments allow for a positive or negative step size, which lets you count up and down and
+// get non-consecutive ranges.
+//
+// Use in a for loop like this:
+//	for x := range iterate.Count(10)
+//	...
 func Count(arg ...int) []int {
 	a := []int{}
 	switch len(arg) {
